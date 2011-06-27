12602fa75db255af0a057b8296769504542fc56b
Craig Vella <CraigVella@gmail.com>
2011-06-27 14:45:22 -0400

    - Fixed, made the new table altertions impossible to be null incase someone ran the foobared update of 1.2

--- src/com/ChaseHQ/Statistician/Database/SQLPatches/stats_v3.sql
+++ src/com/ChaseHQ/Statistician/Database/SQLPatches/stats_v3.sql
@@ -6,25 +6,25 @@
 -- Fixes for `blocks` table
 -- ------------------------
 
-ALTER TABLE `blocks` CHANGE `num_destroyed` `num_destroyed` BIGINT UNSIGNED DEFAULT '0';
-ALTER TABLE `blocks` CHANGE `num_placed` `num_placed` BIGINT UNSIGNED DEFAULT '0';
+ALTER TABLE `blocks` CHANGE `num_destroyed` `num_destroyed` BIGINT UNSIGNED DEFAULT '0' NOT NULL;
+ALTER TABLE `blocks` CHANGE `num_placed` `num_placed` BIGINT UNSIGNED DEFAULT '0' NOT NULL;
 
 -- ------------------------
 -- Fixes for `pickup_drop` table
 -- ------------------------
 
-ALTER TABLE `pickup_drop` CHANGE `num_pickedup` `num_pickedup` BIGINT UNSIGNED DEFAULT '0';
-ALTER TABLE `pickup_drop` CHANGE `num_dropped` `num_dropped` BIGINT UNSIGNED DEFAULT '0';
+ALTER TABLE `pickup_drop` CHANGE `num_pickedup` `num_pickedup` BIGINT UNSIGNED DEFAULT '0' NOT NULL;
+ALTER TABLE `pickup_drop` CHANGE `num_dropped` `num_dropped` BIGINT UNSIGNED DEFAULT '0' NOT NULL;
 
 -- ------------------------
 -- Fixes for `players` table
 -- ------------------------
 
-ALTER TABLE `players` CHANGE `num_secs_loggedon` `num_secs_loggedon` BIGINT UNSIGNED DEFAULT '0';
-ALTER TABLE `players` CHANGE `distance_traveled` `distance_traveled` BIGINT UNSIGNED DEFAULT '0';
-ALTER TABLE `players` CHANGE `distance_traveled_in_minecart` `distance_traveled_in_minecart` BIGINT UNSIGNED DEFAULT '0';
-ALTER TABLE `players` CHANGE `distance_traveled_in_boat` `distance_traveled_in_boat` BIGINT UNSIGNED DEFAULT '0';
-ALTER TABLE `players` CHANGE `distance_traveled_on_pig` `distance_traveled_on_pig` BIGINT UNSIGNED DEFAULT '0';
+ALTER TABLE `players` CHANGE `num_secs_loggedon` `num_secs_loggedon` BIGINT UNSIGNED DEFAULT '0' NOT NULL;
+ALTER TABLE `players` CHANGE `distance_traveled` `distance_traveled` BIGINT UNSIGNED DEFAULT '0' NOT NULL;
+ALTER TABLE `players` CHANGE `distance_traveled_in_minecart` `distance_traveled_in_minecart` BIGINT UNSIGNED DEFAULT '0' NOT NULL;
+ALTER TABLE `players` CHANGE `distance_traveled_in_boat` `distance_traveled_in_boat` BIGINT UNSIGNED DEFAULT '0' NOT NULL;
+ALTER TABLE `players` CHANGE `distance_traveled_on_pig` `distance_traveled_on_pig` BIGINT UNSIGNED DEFAULT '0' NOT NULL;
 
 -- ---------------------------
 -- Update DBVersion To 3
